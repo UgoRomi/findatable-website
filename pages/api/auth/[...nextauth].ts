76b506c688d671f97bd5180a739069aff961948d
Ugo Romi <ugo.romi@gmail.com>
2022-05-10 01:17:36 +0200

    Removed prisma adapter Next auth middleware at the moment only works with JWT session strategy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,9 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
 
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
   theme: {
     colorScheme: 'light',
-  },
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = 'admin';
-      return token;
-    },
   },
   providers: [
     GoogleProvider({
