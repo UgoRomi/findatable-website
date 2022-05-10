3ad2eab0cf504a8bd104e651f4fb42d362887d50
Ugo Romi <ugo.romi@gmail.com>
2022-05-11 01:35:51 +0200

    Removed react-icons and references to Chakra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,6 @@
-import { List, ListIcon, ListItem } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import { MdCheckCircle, MdSettings } from 'react-icons/md';
 const Home: NextPage = () => {
-  return (
-    <List spacing={3}>
-      <ListItem>
-        <ListIcon as={MdCheckCircle} color='green.500' />
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit
-      </ListItem>
-      <ListItem>
-        <ListIcon as={MdCheckCircle} color='green.500' />
-        Assumenda, quia temporibus eveniet a libero incidunt suscipit
-      </ListItem>
-      <ListItem>
-        <ListIcon as={MdCheckCircle} color='green.100' />
-        Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-      </ListItem>
-      {/* You can also use custom icons from react-icons */}
-      <ListItem>
-        <ListIcon as={MdSettings} color='green.500' />
-        Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-      </ListItem>
-    </List>
-  );
+  return <p className='text-xl font-bold'>Welcome!</p>;
 };
 
 export default Home;
